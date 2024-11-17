ce08b44c6a2d5b3ec561ae6bcb3ca4038373aac2
mi358 <mi358@WIN-1.CTI.UGAL.RO>
2024-11-17 17:20:31 +0200

    Al doilea commit

--- src/main/java/ro/ugal/proiectpoo/AparatCuratatGeamuri.java
+++ src/main/java/ro/ugal/proiectpoo/AparatCuratatGeamuri.java
@@ -8,6 +8,46 @@ package ro.ugal.proiectpoo;
  *
  * @author mi358
  */
-public class AparatCuratatGeamuri {
+public class AparatCuratatGeamuri extends Aparat {
+    String firma;
+    String materialLama;
+    int putere;
+    int autonomie;//durata de timp in care nu necesita incarcare
+    int capacitateRezervor;
+
+    //constructor implicit
+    public AparatCuratatGeamuri() {
+        super();
+        firma="Necunoscuta";
+        materialLama="Necunoscut";
+        putere=0;
+        autonomie=0;
+        capacitateRezervor=0;
+    }
+    //constructor cu parametrii
+
+    public AparatCuratatGeamuri(String firma, String materialLama, int putere, int autonomie, int capacitateRezervor, String modAlimentare, String tipActionare, int greutate, String modUtilizare, String tipSuprafata) {
+        super(modAlimentare, tipActionare, greutate, modUtilizare, tipSuprafata);
+        this.firma = firma;
+        this.materialLama = materialLama;
+        this.putere = putere;
+        this.autonomie = autonomie;
+        this.capacitateRezervor = capacitateRezervor;
+    }
+    //constructor de copiere
+
+    public AparatCuratatGeamuri( AparatCuratatGeamuri other) {
+        super(other);
+        this.firma = other.firma;
+        this.materialLama = other.materialLama;
+        this.putere = other.putere;
+        this.autonomie = other.autonomie;
+        this.capacitateRezervor = other.capacitateRezervor;
+    }
+
+    @Override
+    public String toString() {
+        return "AparatCuratatGeamuri:" +super.toString() + "firma=" + firma + ", materialLama=" + materialLama + ", putere=" + putere + ", autonomie=" + autonomie + ", capacitateRezervor=" + capacitateRezervor + '}';
+    }
     
 }
--- src/main/java/ro/ugal/proiectpoo/AparatCuratatPardoseli.java
+++ src/main/java/ro/ugal/proiectpoo/AparatCuratatPardoseli.java
@@ -8,10 +8,48 @@ package ro.ugal.proiectpoo;
  *
  * @author mi358
  */
-public class AparatCuratatPardoseli {
+public class AparatCuratatPardoseli extends Aparat {
     int putereMotor;
     int capacitateRezervor;
+    int lungimeCablu;
     String functii;
+    String accesorii;
+    
+    //constructor implicit
+
+    public AparatCuratatPardoseli() {
+        super();
+        putereMotor=0;
+        capacitateRezervor=0;
+        lungimeCablu=0;
+        functii="Necunoscut";
+        accesorii="Necunoscut";
+    }
+    //constructor cu parametrii
+
+    public AparatCuratatPardoseli(int putereMotor, int capacitateRezervor, int lungimeCablu, String functii, String accesorii, String modAlimentare, String tipActionare, int greutate, String modUtilizare, String tipSuprafata) {
+        super(modAlimentare, tipActionare, greutate, modUtilizare, tipSuprafata);
+        this.putereMotor = putereMotor;
+        this.capacitateRezervor = capacitateRezervor;
+        this.lungimeCablu = lungimeCablu;
+        this.functii = functii;
+        this.accesorii = accesorii;
+    }
+    //constructor de copiere
+
+    public AparatCuratatPardoseli(AparatCuratatPardoseli other) {
+        super(other);
+        this.putereMotor = other.putereMotor;
+        this.capacitateRezervor = other.capacitateRezervor;
+        this.lungimeCablu = other.lungimeCablu;
+        this.functii = other.functii;
+        this.accesorii = other.accesorii;
+    }
+
+    @Override
+    public String toString() {
+        return "AparatCuratatPardoseli:" +super.toString()+ "putereMotor=" + putereMotor + ", capacitateRezervor=" + capacitateRezervor + ", lungimeCablu=" + lungimeCablu + ", functii=" + functii + ", accesorii=" + accesorii + '}';
+    }
     
     
 }
