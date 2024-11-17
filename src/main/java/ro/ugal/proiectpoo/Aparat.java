602f2df10c3a485a638dbd9d3f23e2c94a24098a
mi358 <mi358@WIN-1.CTI.UGAL.RO>
2024-11-17 15:53:07 +0200

    Primul commit

---
+++ src/main/java/ro/ugal/proiectpoo/Aparat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.ugal.proiectpoo;
+
+/**
+ *
+ * @author mi358
+ */
+public class Aparat {
+    String modAlimentare;
+    String tipActionare; //manual, semi-automat, automat
+    int greutate;
+    
+    
+}
---
+++ src/main/java/ro/ugal/proiectpoo/AparatCuratatPardoseli.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.ugal.proiectpoo;
+
+/**
+ *
+ * @author mi358
+ */
+public class AparatCuratatPardoseli {
+    int putereMotor;
+    int capacitateRezervor;
+    String functii;
+    
+    
+}
