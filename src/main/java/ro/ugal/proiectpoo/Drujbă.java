4e7b2f4522012576b344fe941edb67fd133458d6
andreea55 <48280408+andreea55@users.noreply.github.com>
2024-11-18 21:56:47 +0200

    commit: adaugare clase drujba si trimmer

---
+++ src/main/java/ro/ugal/proiectpoo/Drujbă.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.ugal.proiectpoo;
+
+/**
+ *
+ * @author deeac
+ */
+class Drujbă {
+
+    Drujbă(String benzină, String automat, int i, String tăiere, String pardoseală, int i0, String benzină0, String stihl) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/ro/ugal/proiectpoo/Trimmer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.ugal.proiectpoo;
+
+/**
+ *
+ * @author deeac
+ */
+public class Trimmer {
+
+    Trimmer(String cablu, String manual, int i, String tuns_iarba, String iarbă, boolean b, int i0, String black__Decker) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
